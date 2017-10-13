ca6c23faf17a7959a0e83331dc84b2aa3e015611
Rank-0 <841412996@qq.com>
2017-10-13 23:57:56 +0800

    add more in forward_list.cpp

--- forward_list.cpp
+++ forward_list.cpp
@@ -1,42 +1,93 @@
 #include<memory>
+#include<initializer_list>
+#include<utility>
 
 
-
-template<typename Ty,typename Alloc=std::allocator<Ty>>
+template<typename _Ty>
 class foward_list
 {
 
-
-	using value_type = Ty;
-	using iterator = Ty*;
-	using const_iterator = const Ty *;
-	using size_type = unsigned;
-	using difference_type = int;
-
-	using allocator_type = Alloc;
-
-private:
-	iterator _First = nullptr;
-	allocator_type alloc;
 	struct _Node
 	{
 		_Node* next = nullptr;
-		value_type val;
-	} _HeadNode;
+		_Ty val;
+	};
+	using value_type = _Ty;
+	using iterator = _Ty*;
+	using const_iterator = const _Ty *;
+	using size_type = unsigned;
+	using difference_type = int;
+	using allocator_type = std::allocator<_Node>;
+	using _Node_pointer = _Node*;
+
+private:
+	_Node* _Head = nullptr;
 
 public:
 
 	forward_list()
 	{   }
 
-	foward_list(size_type _Size, const Alloc& _Alloc)
+	forward_list(size_type _Size, const _Ty _Val)
+	{
+		_Construc_n(_Size, _Val);
+	}
+
+	forward_list(const forward_list& _Right)
+	{
+		_Node_pointer Rp = _Right.begin();
+		_Node_pointer temp = nullptr;
+		while (Rp)
+		{
+			_Node_pointer p = new _Node;
+			p->val = Rp->val;
+			if (!_Head)
+				_Head =temp= p;
+			else:
+			{
+				temp->next = p;
+				temp = p;
+			}
+			Rp = Rp->next;
+		}
+	}
+
+	forward_list(forward_list&& _Right)
+	{
+		_Head = std::move(_Right._GetHead());
+	}
+
+	template<typename Iter>
+	forward_list(Iter begin, Iter end)
 	{
 
 	}
 
-	forward_list(size_type _Size, const Ty& _Val)
+	forward_list(std::initializer_list<_Ty> il)
 	{
 
+	}
+
+	void _Construct_n(size_type _Size, const _Ty _Val)
+	{
+		_Node_pointer temp = nullptr;
+		for (int i = 0; i != _Size; ++i)
+		{
+			_Node_pointer p = new _Node;
+			p->val = _Val;
+			if (!_Head)
+				_Head =temp= p;
+			else:
+			{
+				temp->next = p;
+				temp = p;
+			}
+		}
+	}
+
+	_Node_pointer& _GetHead()
+	{
+		return _Head;
 	}
 
 
@@ -46,9 +97,12 @@ public:
 
 
 
-	push_front(Ty v)
+	void push_front(const _Ty _Val)
 	{
-
+		_Node_pointer p = new _Node;
+		p->val = _Val;
+		p->next = _Head;
+		_Head = p;
 	}
 
 
