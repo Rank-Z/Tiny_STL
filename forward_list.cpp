81cf0d5eaf029d2368cef87e449c8070a75ae3d4
Rank-0 <841412996@qq.com>
2017-10-14 23:58:26 +0800

    finished insert_after() in forward_list.cpp and fix some BUGs

--- forward_list.cpp
+++ forward_list.cpp
@@ -4,13 +4,19 @@
 
 
 template<typename _Ty>
-class foward_list
+class forward_list
 {
 
 	struct _Node
 	{
 		_Node* next = nullptr;
 		_Ty val;
+
+		_Node(_Node* n, _Ty v)
+		{
+			next = n;
+			val = v;
+		}
 	};
 	using value_type = _Ty;
 	using iterator = _Ty*;
@@ -36,18 +42,16 @@ public:
 	forward_list(const forward_list& _Right)
 	{
 		_Node_pointer Rp = _Right.begin();
-		_Node_pointer temp = nullptr;
+		if(Rp):
+		{
+			_Node_pointer temp =_BuyNode(nullptr, Rp->val);
+			_Head = temp;
+			Rp = Rp->next;
+		}
 		while (Rp)
 		{
-			_Node_pointer p = new _Node;
-			p->val = Rp->val;
-			if (!_Head)
-				_Head =temp= p;
-			else:
-			{
-				temp->next = p;
-				temp = p;
-			}
+			temp->next = _BuyNode(nullptr, Rp->val);
+			temp = temp->next;
 			Rp = Rp->next;
 		}
 	}
@@ -60,42 +64,84 @@ public:
 	template<typename Iter>
 	forward_list(Iter begin, Iter end)
 	{
-
+		if (begin == end)
+			return;
+		_Head = new _Node(nullptr, *begin);
+		_Node_pointer temp = _Head;
+		for (++begin; begin != end; ++begin)
+		{
+			temp->next = new _Node(nullptr, *begin);
+			temp = temp->next;
+		}
 	}
 
-	forward_list(std::initializer_list<_Ty> il)
+	forward_list(std::initializer_list<_Ty> il):forward_list(il.begin(),il.end())
+	{   }
+
+	forward_list& operator=(const forward_list& _Right)
 	{
-
+		_Node_pointer Rp = _Right.begin();
+		if (Rp) :
+		{
+			_Node_pointer temp = _BuyNode(nullptr, Rp->val);
+			_Head = temp;
+			Rp = Rp->next;
+		}
+		while (Rp)
+		{
+			temp->next = _BuyNode(nullptr, Rp->val);
+			temp = temp->next;
+			Rp = Rp->next;
+		}
 	}
+
+/*********************************************************************************/
 
 	void _Construct_n(size_type _Size, const _Ty _Val)
 	{
 		_Node_pointer temp = nullptr;
 		for (int i = 0; i != _Size; ++i)
-		{
-			_Node_pointer p = new _Node;
-			p->val = _Val;
-			if (!_Head)
-				_Head =temp= p;
-			else:
-			{
-				temp->next = p;
-				temp = p;
-			}
-		}
+			temp = _BuyNode(temp, _Val);
+		_Head = temp;
+	}
+	template<typename..._Args>
+	_Node_pointer _BuyNode(_Node_pointer addr,_Args..._args)
+	{
+		return new _Node(addr,std::forward(_args)...);
 	}
 
-	_Node_pointer& _GetHead()
+	_Node_pointer& _GetHead()const
 	{
 		return _Head;
 	}
 
+/*********************************************************************************/
+	
+	_Node_pointer begin()const
+	{
+		return _Head;
+	}
 
+	const _Node_pointer	cbegin()const
+	{
+		return _Head;
+	}
 
+	_Node_pointer end()const
+	{
+		return nullptr;
+	}
 
+	template<typename..._Args>
+	_Node_pointer _Insert_after(_Node_pointer _Where, _Args&&..._args)
+	{
+		return _Where->next=_BuyNode(_Where->next, std::forward(_args)...);
+	}
 
-
-
+	const _Node_pointer cend()const
+	{
+		return nullptr;
+	}
 
 	void push_front(const _Ty _Val)
 	{
@@ -105,5 +151,41 @@ public:
 		_Head = p;
 	}
 
+	template<typename..._Args>
+	auto emplace_front(_Args&&..._args)
+	{
+		_Head = _BuyNode(nullptr, std::forward(_args)...);
+	}
 
+	_Node_pointer insert_after(const _Node_pointer _Where, const _Ty _Val)
+	{
+		return _Insert_after(_Where, _Val);
+	}
+
+	_Node_pointer insert_after(const _Node_pointer _Where, const size_type _Count, const _Ty _Val)
+	{
+		_Node_pointer temp = _Where->next;
+		for (int i = 0; i != _Count, ++i)
+		{
+			temp = _Insert_after(temp, _Val);
+		}
+		return temp;
+	}
+
+	template<typename Iter>
+	_Node_pointer insert_after(const _Node_pointer _Where, Iter begin, Iter end)
+	{
+		//assuem Iter support operator -- and begin is always less than end 
+		_Node_pointer temp = _Where->next;
+		for (Iter it = end; p >= begin; --it)
+		{
+			temp = _Insert_after(temp, *it);
+		}
+		return temp;
+	}
+
+	_Node_pointer insert_after(const _Node_pointer _Where,const std::initializer_list<_Ty> il)
+	{
+		return insert_after(_Where, il.begin(), il.end());
+	}
 };
