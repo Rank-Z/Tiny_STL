d9761b7333d1dd279144ab0cac7e5229c85bd336
Grt <841412996@qq.com>
2017-11-05 23:00:44 +0600

    add more in list.cpp

--- list.cpp
+++ list.cpp
@@ -61,6 +61,20 @@ struct _list_iterator
 		--*this;
 		return tmp;
 	}
+
+	iterator operator+(int i)
+	{
+		for (; i != 0; --i)
+			++*this;
+		return *this;
+	}
+
+	iterator operator-(int i)
+	{
+		for (; i != 0; --i)
+			--*this;
+		return *this;
+	}
 };
 
 template<typename _Ty>
@@ -112,9 +126,23 @@ struct _list_const_iterator
 		--*this;
 		return tmp;
 	}
+
+	const_iterator operator+(int i)
+	{
+		for (; i != 0; --i)
+			++*this;
+		return *this;
+	}
+
+	const_iterator operator-(int i)
+	{
+		for (; i != 0; --i)
+			--*this;
+		retunr *this;
+	}
 };
 
-template<_Ty>
+template<typename _Ty>
 struct simple_alloc
 {
 	using pointer = _Ty*;
@@ -133,7 +161,7 @@ template<typename _Ty,typename _Alloc=simple_alloc<_list_node<_Ty>>>
 class list
 {
 public:
-	using list_node = _list_node;
+	using list_node = _list_node<_Ty>;
 	using list_node_allocator = _Alloc;
 	using value_type = _Ty;
 	using pointer = value_type*;
@@ -159,6 +187,11 @@ private:
 		list_node_allocator::deallocate(p);
 	}
 
+	void _Reset()
+	{
+		(*node).next = node;
+		(*node).prev = node;
+	}
 
 public:
 
@@ -167,6 +200,76 @@ public:
 		(*node).next = node;
 		(*node).prev = node;
 	}
+
+	list(size_type _Count, const _Ty& _Val=_Ty()) :length(_Count),node(_Buynode())
+	{
+		iterator now = _Buynode();
+		*now = _Val;
+		(*now.node).prev = node;
+		(*node).next = now.node;
+		for (--_Count; _Count; --_Count)
+		{
+			iterator tmp = _Buynode();
+			*tmp = _Val;
+			(*now.node).next = tmp.node;
+			(*tmp.node).prev = now.node;
+			now = tmp;
+		}
+		(*now.node).next = node;
+		(*node).prev = now.node;
+	}
+
+	template<typename Iter>
+	list(Iter _First, Iter _Last):node(_Buynode())
+	{
+		if (_First == _Last)
+		{
+			_Reset();
+			return;
+		}
+		iterator now = _Buynode();
+		++length;
+		*now = *_First;
+		(*now.node).prev = node;
+		(*node).next = now.node;
+		for (++_First; _First != _Last; ++_First)
+		{
+			iterator tmp = _Buynode();
+			++length;
+			*tmp = *_First;
+			(*now.node).next = tmp.node;
+			(*tmp.node).prev = now.node;
+			now = tmp;
+		}
+		(*now.node).next = node;
+		(*node).prev = now.node;
+	}
+
+	list(const list<_Ty>& _Right):list(_Right.begin(),_Right.end()){ }
+
+	list(const list<_Ty>&& _Right):length(_Right.length),node(_Buynode())
+	{
+		(*node).prev = (*(_Right.node)).prev;
+		(*node).next = (*(_Right.node)).next;
+		(*static_cast<link_type>((*(_Right.node)).prev)).next = node;
+		(*static_cast<link_type>((*(_Right.node)).next)).prev = node;
+		_Right.length = 0;
+		_Right._Reset();
+	}
+
+	~list()
+	{
+		iterator it1(node);
+		iterator it2((*node).next);
+		for (++length; length; --length)
+		{
+			_Freenode(it1.node);
+			it1 = it2;
+			++it2;
+		}
+	}
+
+
 
 	iterator begin()
 	{
@@ -215,7 +318,156 @@ public:
 
 	reference back()
 	{
+		return *(--(static_cast<iterator>(end)));
+	}
 
+	const_reference back() const
+	{
+		return *(--(static_cast<const_iterator>(end)));
+	}
+
+	void swap(list<_Ty> _Right)
+	{
+		link_type tmp1 = node;
+		node = _Right.node;
+		_Right.node = tmp1;
+
+		size_type tmp2 = length;
+		length = _Right.length;
+		_Right.length = tmp2;
+	}
+
+	iterator insert(iterator _Pos, const _Ty& _Val=_Ty())
+	{
+		link_type tmp = _Buynode();
+		(*tmp).data = _Val;
+		(*tmp).next = _Pos.node;
+		(*tmp).prev = (*_Pos.node).prev;
+		(*static_cast<link_type>((*_Pos.node).prev)).next = tmp;
+		(*_Pos.node).prev = tmp;
+		++length;
+		return tmp;
+	}
+
+	iterator insert(iterator _Pos, size_type _Count, const _Ty& _Val)
+	{
+		if (_Count == 1)
+			return insert(_Pos, _Val);
+		link_type first = _Buynode();
+		link_type last = _Buynode();
+		(*first).prev = (*_Pos.node).prev;
+		(*last).next = _Pos.node;
+		(*static_cast<link_type>((*_Pos.node).prev)).next = first;
+		(*_Pos.node).prev = last;
+		link_type now = first;
+		for (int i = _Count - 2; i != 0; --i)
+		{
+			link_type tmp = _Buynode();
+			(*now).next = tmp;
+			(*tmp).prev = now;
+			now = tmp;
+		}
+		(*now).next = last;
+		(*last).prev = now;
+		length += _Count;
+		return first;
+	}
+
+	template<typename Iter>
+	iterator insert(iterator _Pos,Iter _First, Iter _Last)
+	{
+		if (_First == _Last)
+			return _Pos;
+		link_type first = _Buynode();
+		++length;
+		(*first).data = *_First;
+		(*static_cast<link_type>((*_Pos.node).prev)).next = first;
+		(*first).prev = (*_Pos.node).prev;
+		link_type now = first;
+		for (; ++_First != _Last;)
+		{
+			link_type tmp = _Buynode();
+			(*now).next = tmp;
+			(*tmp).prev = now;
+			now = tmp;
+			++length;
+		}
+		(*now).next = _Pos.node;
+		(*_Pos.node).prev = now;
+		return first;
+	}
+
+	void push_front(const _Ty& _Val)
+	{
+		insert(begin(), _Val);
+	}
+
+	void push_back(const _Ty& _Val)
+	{
+		insert(node, _Val);
+	}
+
+	iterator erase(iterator _Pos)
+	{
+		(*static_cast<link_type>((*_Pos.node).prev)).next = (*_Pos.node).next;
+		(*static_cast<link_type>((*_Pos.node).next)).prev = (*_Pos.node).prev;
+		iterator res (static_cast<link_type>((*_Pos.node).next));
+		_Freenode(_Pos);
+		--length;
+		return res;
+	}
+
+	iterator erase(iterator _First, iterator _Last)
+	{
+		(*static_cast<link_type>((*_First.node).prev)).next = _Last.node;
+		(*_Last.node).prev = (*_First.node).prev;
+		for (; _First != _Last; ++_First)
+		{
+			_Freenode(_First);
+			--length;
+		}
+		return _Last;
+	}
+	
+	void resize(size_type _Newsize, const _Ty& _Val=_Ty())
+	{
+		if (_Newsize == length)
+			return;
+		else if (_Newsize < length)
+		{
+			difference_type dif = length - _Newsize;
+			iterator it = begin();
+			it += _Newsize;
+			for (; it != node; ++it)
+				_Freenode(it);
+			it = begin() + (_Newsize - 1);
+			(*it.node).next = node;
+			(*node).prev = it.node;
+			length = _Newsize;
+		}
+		else
+		{
+			difference_type dif = _Newsize - length;
+			for (; dif != 0; --dif)
+				push_back(_Val);
+		}
+	}
+
+	void clear()
+	{
+		for (iterator it = begin(); it != end(); ++it)
+			_Alloc::deallocate(it.node);
+		_Reset();
+	}
+
+	void  pop_front()
+	{
+		erase(begin());
+	}
+
+	void pop_back()
+	{
+		erase(--end());
 	}
 
 
