43180cb694bc347542b6dffcb192a607c4fdb976
Grt <841412996@qq.com>
2018-01-06 20:33:56 +0800

    add ordered set  in "set.hpp"

--- Containers/include/RB_Tree.hpp
+++ Containers/include/RB_Tree.hpp
@@ -1,74 +1,239 @@
-#ifndef RB_TREE_HPP
-#define RB_TREE_HPP
+#ifndef CONTAINERS_REDBLACKTREE_HPP
+#define CONTAINERS_REDBLACKTREE_HPP
+
+#include<functional>
+#include<memory>
+#include<utility>
+#define _STD ::std::
+
 template<typename Key>
 class RB_node
 {
 public:
+	using key_type = Key;
+	using value_type = key_type;
 
 	RB_node(const Key&key , RB_node* p = nullptr , RB_node* l = nullptr , RB_node* r = nullptr)
 		:k(key) , par(p) , left(l) , right(r)
 	{   }
 
+	RB_node()
+		:k(Key()) , par(nullptr) , left(nullptr) , right(nullptr) , isred(false)
+	{   }
 
-
+	Key& getkey()
+	{
+		return k;
+	}
 
 	Key k;
 	bool isred;
 	RB_node* par;
 	RB_node* left;
 	RB_node* right;
-};
+};//TODO:delete
 
 
-template<typename Key>
-class RB_Tree
+
+
+template<typename nodetype ,
+	typename Itertype ,
+	typename Compare ,
+	typename Allocator>
+	class RB_Tree
 {
 public:
+	using Key = typename nodetype::key_type;
 	using key_type = Key;
-	using nodetype = RB_node<key_type>;
+	using value_type = typename nodetype::value_type;
 	using nodeptr = nodetype * ;
+	using allocator_type = Allocator;
+	using iterator = Itertype;
+	using key_compare = Compare;
+	using ketptr = key_type * ;
 
-	RB_Tree& insert(key_type key)
+	RB_Tree()
 	{
-		nodeptr parent = root;
-		nodeptr np = new nodetype (key);
+		init()
+	}
 
-		if (root != nullptr)
+	RB_Tree(const Compare& comp,const Allocator& alloc=Allocator())
+		:_compare(comp),_alloc(alloc)
+	{
+		init()
+	}
+
+	RB_Tree(const Allocator& alloc)
+		:_alloc(alloc)
+	{
+		init();
+	}
+
+	RB_Tree(const RB_Tree& right,const Allocator& alloc)
+		:_compare(right.key_comp()),_alloc(alloc)
+	{
+		init();
+		nodeptr np = right.first();
+		for (; np != right.getroot(); np = right.successor(np))
+			insert_multiable(np->getkey());
+	}
+
+	RB_Tree(const RB_Tree& right)
+		:RB_Tree(right,
+			_STD allocator_traits<Allocator>::select_on_container_copy_construction(right.get_allocator()))
+	{   }
+
+	RB_Tree(RB_Tree&& right)
+		:_compare(_STD move(right.key_comp())),_alloc(_STD move(right.get_allocator())),
+		nilnode(_STD move(*(right.getnil())))
+	{
+		init();
+	}
+
+	RB_Tree(RB_Tree&& right , const Allocator& alloc)
+		:RB_Tree(right)
+	{
+		_alloc = alloc;
+	}
+
+	RB_Tree& operator=(const RB_Tree& right)
+	{
+		clear();
+		nodeptr np = right.first();
+		for (; np != right.getroot(); np = right.successor(np))
+			insert_multiable(np->getkey());
+		return *this;
+	}
+
+	RB_Tree& operator=(RB_Tree&& right)
+	{
+		clear();
+		nilnode = _STD move(right.nilnode);
+		nil = right.nil;
+		root = right.root;
+		_compare = _STD move(right._compare);
+		_alloc = _STD move(right._alloc);
+		
+		return *this;
+	}
+
+	void swap(RB_Tree& right)
+	{
+
+	}
+
+	~RB_Tree()
+	{
+		clear();
+	}
+
+	nodeptr getroot()
+	{
+		return root;
+	}
+
+	nodeptr getnil()
+	{
+		return nil;
+	}
+
+	bool empty()
+	{
+		return (root == nil);
+	}
+
+	iterator begin() const
+	{
+		nodeptr np = first();
+		if (np == nullptr)
+			np = nil;
+
+		iterator it(np , this);
+		return it;
+	}
+
+	iterator end() const
+	{
+		iterator it(nil , this);
+		return it;
+	}
+
+	int back_height() const
+	{
+		nodeptr np = root;
+		int back_count = 0;
+		for (; np != nil;)
 		{
-			for (;;)
-			{
-				if (key < parent->k)
-				{
-					if (parent->left)
-						parent = parent->left;
-					else
-						break;
-				}
-				else
-				{
-					if (parent->right)
-						parent = parent->right;
-					else
-						break;
-				}
-			}
+			if (!np->isred)
+				++back_count;
+			np = np->left();
+		}
+		return back_count;
+	}
 
-			np->par = parent;
-			if (key < parent->k)
-				parent->left = np;
+	void insert_multiable(const key_type& key)
+	{
+		nodeptr np = buynode(key);
+		insert(np);
+	}
+
+	_STD pair<iterator,bool> insert_unique(const key_type& key)
+	{
+		nodeptr np = find(key);
+		bool ret = false;
+		if (np == nullptr)
+		{
+			np = buynode(key);
+			insert(np);
+			ret = true;
+		}
+
+		return _STD make_pair(make_iter(np) , ret);
+	}
+
+	_STD pair<iterator , bool> insert_unique(key_type&& key)
+	{
+		nodeptr np = find(key);
+		bool ret = false;
+		if (np == nullptr)
+		{
+			np = buynode_rvalue(key);
+			insert(np);
+			ret = true;
+		}
+
+		return _STD make_pair(make_iter(np) , ret);
+	}
+
+	void insert(nodeptr np)
+	{
+		nodeptr parent = nil;
+		for (nodeptr temp = root; temp != nil;)
+		{
+			parent = temp;
+			if (np->getkey() < temp->getkey())
+				temp = temp->left;
 			else
-				parent->right = np;
+				temp = temp->right;
 		}
-		else
-		{
+		np->par = parent;
+		if (parent == nil)
 			root = np;
-			np->par = nullptr;
-		}
+		else if (np->getkey() < parent->getkey())
+			parent->left = np;
+		else
+			parent->right = np;
+		np->right = nil;
+		np->left = nil;
 		np->isred = true;
-		np->left = np->right = nullptr;
 		insert_fixup(np);
 
-		return *this;
+		++_size;
+	}
+
+	void quick_insert(nodeptr hint , nodeptr np)
+	{
+
 	}
 
 	nodeptr find(const key_type& key) const
@@ -76,36 +241,103 @@ public:
 		nodeptr now = root;
 		for (; now;)
 		{
-			if (key == now->k)
+			if ((!_compare(key , now->getkey()))&& (!_compare(now->getkey() , key)))
 				break;
-			else if (key < now->k)
+			else if (_compare(key,now->getkey()))
 				now = now->left;
 			else
 				now = now->right;
 		}
-		return now;
+		if (now != nil)
+			return now;
+		else
+			return nullptr;
 	}
 
-	RB_Tree& deletekey(const key_type& key)
+	int find_or_parent(const key_type& key,nodeptr& now) const
+	{
+		if (root == nil)
+			return nullptr;
+
+		now = root;
+		for (;;)
+		{
+			if ((!_compare(key , now->getkey())) && (!_compare(now->getkey() , key)))
+				return 0;
+			else if (_compare(key , now->getkey()))
+			{
+				if (now->left == nil)
+					return 1;
+				else
+					now = now->left;
+			}
+			else
+			{
+				if (now->right == nil)
+					return -1;
+				else
+					now = now->right;
+			}
+		}
+	}
+
+	iterator lower_bound(const key_type& key) const
+	{
+		nodeptr np;
+		switch (find_or_parent(key , np))
+		{
+		case 0:
+		case 1:
+			return make_iter(np);
+		case -1:
+			return ++make_iter(np);
+		}
+	}
+
+	iterator upper_bound(const key_type& key) const
+	{
+		nodeptr np;
+		switch (find_or_parent(key , np))
+		{
+		case 1:
+			return make_iter(np);
+		case 0:
+		case -1:
+			return ++make_iter(np);
+		}
+	}
+
+	bool deletekey(const key_type& key)
 	{
 		nodeptr np = find(key);
 		if (np)
+		{
 			erase(np);
-
-		return *this;
+			return true;
+		}
+		else
+			return false;
 	}
 
-	RB_Tree& erase(nodeptr z)
+	iterator erase(iterator pos)
+	{
+		iterator ret(pos);
+		++ret;
+		erase(pos.node);
+		return ret;
+	}
+
+	void erase(nodeptr z)
 	{
 		nodeptr x;
 		nodeptr y = z;
 		bool y_original_color = y->isred;
-		if (z->left == nullptr)
+		if (z->left == nil)
 		{
 			x = z->right;
 			transplant(z , z->right);
 		}
-		else if (z->right == nullptr)
+		else if (z->right == nil)
 		{
 			x = z->left;
 			transplant(z , z->left);
@@ -130,14 +362,105 @@ public:
 		}
 		if (y_original_color == false)
 			delete_fixup(x);
-		return *this;
+
+		freenode(z);
+		--_size;
+	}
+
+	void clear()
+	{
+		cleartree(root);
+		root = nil;
+	}
+
+	nodeptr successor(nodeptr np) const
+	{
+		if (np->right != nil)
+			return minimum(np->right);
+
+		nodeptr parent = np->par;
+		for (; parent != nil && np == parent->right;)
+		{
+			np = parent;
+			parent = parent->par;
+		}
+
+			return parent;
+	}
+
+	nodeptr predecessor(nodeptr np) const
+	{
+		if (np->left != nil)
+			return maxmum(np->left);
+
+		nodeptr parent = np->par;
+		for (; parent != nil && np == parent->left)
+		{
+			np = parent;
+			parent = parent->par;
+		}
+
+			return parent;
+	}
+
+	nodeptr first() const
+	{
+			return minimum(root);
+	}
+
+	nodeptr last() const
+	{
+			return maxmum(root);
+	}
+
+	unsigned size() const noexcept
+	{
+		return _size;
+	}
+
+	Allocator& get_allocator()
+	{
+		return _alloc;
+	}
+
+	Compare& key_comp()
+	{
+		return _compare;
+	}
+
+	iterator make_iter(nodeptr np) const
+	{
+		iterator it(np , this);
+		return it;
 	}
 
 private:
 
+	template<typename ...Args>
+	nodeptr buynode(Args&&...args)
+	{
+		nodeptr np = new nodetype;
+		np->kp = _alloc.allocate(1);
+		_alloc.construct(np->kp ,_STD forward<Args>(args)...);
+		return np;
+	}
+
+	nodeptr buynode_rvalue(key_type&& key)
+	{
+		nodeptr np = new nodeptr;
+		np->kp = &key;
+		return np;
+	}
+
+	void freenode(nodeptr np)
+	{
+		_alloc.deallocate(np->kp,1);
+		delete np;
+	}
+
 	void insert_fixup(nodeptr np)
 	{
-		for (; np->par&&np->par->isred;)
+		for (; np->par->isred;)
 		{
 
 			nodeptr parbro;
@@ -145,7 +468,7 @@ private:
 			{
 				parbro = np->par->par->right;
 
-				if (parbro&&parbro->isred)
+				if (parbro->isred)
 				{
 					np->par->isred = false;
 					parbro->isred = false;
@@ -167,7 +490,7 @@ private:
 			else
 			{
 				parbro = np->par->par->left;
-				if (parbro&&parbro->isred)
+				if (parbro->isred)
 				{
 					np->par->isred = false;
 					parbro->isred = false;
@@ -186,7 +509,7 @@ private:
 					leftrotate(np->par->par);
 				}
 			}
-		}//for end
+		}
 		root->isred = false;
 	}
 
@@ -195,11 +518,11 @@ private:
 		nodeptr oth = np->right;
 		np->right = oth->left;
 
-		if (oth->left != nullptr)
+		if (oth->left != nil)
 			oth->left->par = np;
 
 		oth->par = np->par;
-		if (np == root)
+		if (np->par == nil)
 			root = oth;
 		else if (np == np->par->left)
 			np->par->left = oth;
@@ -215,11 +538,11 @@ private:
 		nodeptr oth = np->left;
 		np->left = oth->right;
 
-		if (oth->right != nullptr)
+		if (oth->right != nil)
 			oth->right->par = np;
 
 		oth->par = np->par;
-		if (np == root)
+		if (np->par == nil)
 			root = oth;
 		else if (np == np->par->left)
 			np->par->left = oth;
@@ -230,22 +553,21 @@ private:
 		np->par = oth;
 	}
 
-	void transplant(nodeptr& x , nodeptr& y)
+	void transplant(nodeptr x , nodeptr y)
 	{
-		if (x->par == nullptr)
+		if (x->par == nil)
 			root = y;
 		else if (x == x->par->left)
 			x->par->left = y;
 		else
 			x->par->right = y;
 
-		if (y)
-			y->par = x->par;
+		y->par = x->par;
 	}
 
-	void delete_fixup(nodeptr& x)
+	void delete_fixup(nodeptr x)
 	{
-		for (; x != root && (x == nullptr || x->isred == false);)
+		for (; x != root && (x->isred == false);)
 		{
 			nodeptr bro;
 			if (x == x->par->left)
@@ -258,27 +580,25 @@ private:
 					leftrotate(x->par);
 					bro = x->par->right;
 				}
-				if ((bro->left == nullptr || bro->left->isred == false) && (bro->right == nullptr || bro->right->isred == false))
+				if ((bro->left->isred == false) && (bro->right->isred == false))
 				{
 					bro->isred = true;
 					x = x->par;
 				}
 				else
 				{
-					if (bro->right == nullptr || bro->right->isred == false)
+					if (bro->right->isred == false)
 					{
-						if (bro->left)
-							bro->left->isred = false;
+						bro->left->isred = false;
 						bro->isred = true;
 						rightrotate(bro);
 						bro = x->par->right;
 					}
 					bro->isred = x->par->isred;
 					x->par->isred = false;
-					if (bro->right)
-						bro->right->isred = false;
+					bro->right->isred = false;
 					leftrotate(x->par);
-					break;
+					x = root;
 				}
 			}
 			else
@@ -291,39 +611,36 @@ private:
 					rightrotate(x->par);
 					bro = x->par->left;
 				}
-				if ((bro->right == nullptr || bro->right->isred == false) && (bro->left == nullptr || bro->left->isred == false))
+				if ((bro->right->isred == false) && (bro->left->isred == false))
 				{
 					bro->isred = true;
 					x = x->par;
 				}
 				else
 				{
-					if ((bro->left == nullptr) || (bro->left->isred == false))
+					if (bro->left->isred == false)
 					{
-						if (bro->right)
-							bro->right->isred = false;
+						bro->right->isred = false;
 						bro->isred = true;
 						leftrotate(bro);
 						bro = x->par->left;
 					}
 					bro->isred = x->par->isred;
 					x->par->isred = false;
-					if (bro->left)
-						bro->left->isred = false;
+					bro->left->isred = false;
 					rightrotate(x->par);
-					break;
+					x = root;
 				}
 			}
 		}
-		if (x)
-			x->isred = false;
+		x->isred = false;
 	}
 
 	nodeptr minimum(nodeptr np)
 	{
 		for (;;)
 		{
-			if (np->left)
+			if (np->left != nil)
 				np = np->left;
 			else
 				return np;
@@ -341,9 +658,31 @@ private:
 		}
 	}
 
-	nodeptr root = nullptr;
+	void cleartree(nodeptr np)
+	{
+		if (np == nil)
+			return;
+
+		cleartree(np->left);
+		cleartree(np->right);
+		freenode(np);
+	}
+
+	void init()
+	{
+		nil = &nilnode;
+		root = nil;
+		_size = 0;
+	}
+
+
+	nodeptr root;
+	nodetype nilnode;
+	nodeptr nil;
+	allocator_type _alloc;
+	Compare _compare;
+	unsigned _size;
 };
 
 
-
-#endif // !RB_TREE_HPP
+#endif //CONTAINERS_REDBLACKTREE_HPP
---
+++ Containers/set.hpp
@@ -0,0 +1,551 @@
+#include"RB_Tree.hpp"
+#include<memory>
+#include<functional>
+#include<stdexcept>
+#include<iterator>
+#include<algorithm>
+#include<utility>
+#define _STD ::std::
+
+template<typename Key>
+class set_node
+{
+public:
+	using key_type = Key;
+	using value_type = key_type;
+
+	set_node(key_type* k = nullptr , set_node* p = nullptr , set_node* l = nullptr , set_node* r = nullptr)
+		:kp(k) , par(p) , left(l) , right(r) , isred(false)
+	{   }
+
+	key_type& getkey()
+	{
+		return *kp;
+	}
+
+	value_type& getvalue()
+	{
+		return *kp;
+	}
+
+	key_type* kp;
+	bool isred;
+	set_node* par;
+	set_node* left;
+	set_node* right;
+};
+
+template<typename Key ,
+	typename Compare ,
+	typename Allocator>
+	class set_iterator
+{
+public:
+	using key_type = Key;
+	using value_type = key_type;
+
+private:
+
+	using nodetype = set_node<key_type>;
+	using nodeptr = nodetype * ;
+	using treetype = RB_Tree<key_type , set_iterator , Compare , Allocator>;
+	using treeptr = treetype * ;
+	friend class treetype;
+public:
+	set_iterator(nodeptr n , treeptr t)
+		:node(n) , tree(t)
+	{   }
+
+	set_iterator(const set_iterator& right)
+		:node(right.node) , tree(right.tree)
+	{   }
+
+	set_iterator& operator=(const set_iterator& right)
+	{
+		node = right.node;
+		tree = right.tree;
+	}
+
+	const value_type& operator*() const
+	{
+		if (tree->nil == node)
+			throw _STD out_of_range { "set_iterator out_of_range!" };
+		return node->getvalue();
+	}
+
+	set_iterator& operator++()
+	{
+		if (node == tree->getnil())
+			throw _STD out_of_range { "set_iterator out_of_range!" };
+		node = tree->predecessor(node);
+		return *this;
+	}
+
+	set_iterator operator++(int)
+	{
+		set_iterator ret(*this);
+		++(*this);
+		return ret;
+	}
+
+	set_iterator& operator--()
+	{
+		if (node == tree->getnil())
+		{
+			if (tree->empty())
+				throw _STD out_of_range { "set_iterator out_of_range!" };
+			node = tree->last();
+		}
+		else
+			node = tree->successor(node);
+
+		return *this;
+	}
+
+	set_iterator operator--(int)
+	{
+		set_iterator ret(*this);
+		--(*this);
+		return ret;
+	}
+
+	bool operator==(const set_iterator& right) const
+	{
+		return ((node == right.node) && (tree == right.tree));
+	}
+
+	bool operator!=(const set_iterator& right) const
+	{
+		return !(*this == right);
+	}
+
+	nodeptr operator->() const
+	{
+		return node;
+	}
+
+
+private:
+	nodeptr node;
+	treeptr tree;
+
+};
+
+template<typename Key ,
+	typename Compare = _STD less<Key> ,
+	typename Allocator = _STD allocator<Key>>
+	class set
+{
+public:
+	using key_type = Key;
+	using value_type = Key;
+	using size_type = unsigned int;
+	using difference_type = int;
+	using key_compare = Compare;
+	using value_compare = Compare;
+	using allocator_type = Allocator;
+	using reference = value_type & ;
+	using const_reference = const value_type&;
+	using pointer = typename _STD allocator_traits<Allocator>::pointer;
+	using const_pointer = typename _STD allocator_traits<Allocator>::const_pointer;
+	using iterator = set_iterator<value_type , Compare , Allocator>;
+	using const_iterator = iterator;
+	using reverse_iterator = _STD reverse_iterator<iterator>;
+	using const_reverse_iterator = _STD reverse_iterator<const_iterator>;
+	using node_type = set<key_type>;//since C++17
+
+private:
+	using treetype = RB_Tree<set_node<key_type> , iterator , Compare , Allocator>;
+	using nodeptr = node_type * ;
+public:
+	explicit set(const Compare& comp , const Allocator& alloc = Allocator())
+		:_tree(comp , alloc)
+	{   }
+
+	set() :set(Compare())
+	{   }
+
+	explicit set(const Allocator& alloc)
+		:_tree(alloc)
+	{   }
+
+	template<typename Iter>
+	set(Iter first , Iter last ,
+		const Compare& comp = Compare() , const Allocator& alloc = Allocator())
+		: _tree(comp , alloc)
+	{
+		_insert_range(first , last);
+	}
+
+	template<typename Iter>
+	set(Iter first , Iter last ,
+		const Allocator& alloc)
+		: set(first , last , Compare() , alloc)
+	{   }
+
+	set(const set& right)
+		:_tree(right._tree)
+	{   }
+
+	set(const set& right , const Allocator& alloc)
+		:_tree(right._tree , alloc)
+	{   }
+
+	set(set&& right)
+		:_tree(right._tree)
+	{   }
+
+	set(set&& right , const Allocator& alloc)
+		:_tree(right._tree , alloc)
+	{   }
+
+	set(_STD initializer_list<value_type> il ,
+		const Compare& comp = Compare() , const Allocator& alloc = Allocator())
+		:_tree(comp , alloc)
+	{
+		_insert_range(il.begin() , il.end());
+	}
+
+	set(_STD initializer_list<value_type> il , const Allocator& alloc)
+		:set(il , Compare() , alloc)
+	{   }
+
+	~set()
+	{   }
+
+	set& operator=(const set& right)
+	{
+		clear();
+		_tree = right._tree;
+		return *this;
+	}
+
+	set& operator=(set&& right)
+		noexcept(_STD allocator_traits<Allocator>::is_always_equal::value&&_STD is_nothrow_move_assignable<Compare>::value)
+	{
+		clear();
+		_tree = _STD move(right._tree);
+		return *this;
+	}
+
+	set& operator=(_STD initializer_list<value_type> il)
+	{
+		clear();
+		_insert_range(il.begin() , il.end());
+		return *this;
+	}
+
+	allocator_type& get_allocator() const
+	{
+		return _tree.get_allocator();
+	}
+
+	iterator begin() noexcept
+	{
+		return _tree.begin();
+	}
+
+	const_iterator begin() const noexcept
+	{
+		return _tree.begin();
+	}
+
+	const_iterator cbegin() const noexcept
+	{
+		return _tree.begin();
+	}
+
+	iterator end() noexcept
+	{
+		return _tree.end();
+	}
+
+	const_iterator end() const noexcept
+	{
+		return _tree.end();
+	}
+
+	const_iterator cend() const noexcept
+	{
+		return _tree.end();
+	}
+
+	reverse_iterator rbegin() noexcept
+	{
+		return _tree.end();
+	}
+
+	const_reverse_iterator rbegin() const noexcept
+	{
+		return _tree.end();
+	}
+
+	const_reverse_iterator crbegin() const noexcept
+	{
+		return _tree.end();
+	}
+
+	reverse_iterator rend() noexcept
+	{
+		return _tree.begin();
+	}
+
+	const_reverse_iterator rend() const noexcept
+	{
+		return _tree.begin();
+	}
+
+	const_reverse_iterator crend() const noexcept
+	{
+		return _tree.begin();
+	}
+
+	bool empty() const noexcept
+	{
+		return _tree.empty();
+	}
+
+	size_type size() const noexcept
+	{
+		return _tree.size();
+	}
+
+	size_type max_size() const noexcept
+	{
+		return 357913941;
+	}
+
+	void clear()
+	{
+		_tree.clear()
+	}
+
+	_STD pair<iterator , bool> insert(const value_type& value)
+	{
+		return _tree.insert_unique(value);
+	}
+
+	_STD pair<iterator , bool> insert(value_type&& value)
+	{
+		return _tree.insert_unique(value);
+	}
+
+	iterator insert(const_iterator hint , const value_type& value)
+	{
+		if ((!_tree.key_comp()(*hint , value)) && (!_tree.key_comp()(value , *hint)))
+			return hint;
+		else
+			return insert(value);
+	}
+
+	iterator insert(const_iterator hint , value_type&& value)
+	{
+		if ((!_tree.key_comp()(*hint , value)) && (!_tree.key_comp()(value , *hint)))
+			return hint;
+		else
+			return insert(value);
+	}
+
+	template<typename Iter>
+	void insert(Iter first , Iter last)
+	{
+		for (; first != last; ++first)
+			insert(*first);
+	}
+
+	void insert(_STD initializer_list<value_type> il)
+	{
+		insert(il.begin() , il.end());
+	}
+
+	template<typename...Args>
+	_STD pair<iterator , bool> emplace(Args&&...args)
+	{
+		value_type* vp = _tree.get_allocator().allocate(1);
+		_tree.get_allocator().construct(vp , _STD forward<Args>(args)...);
+		_STD pair<iterator , bool> ret(_tree.insert_unique(_STD move(*vp)));
+		if (ret.second == false)
+			_tree.get_allocator().deallocate(vp , 1);
+
+		return ret;
+	}
+
+	template<typename...Args>
+	iterator emplace_hint(const_iterator hint , Args&&...args)
+	{
+		value_type* vp = _tree.get_allocator().allocate(1);
+		_tree.get_allocator().construct(vp , _STD forward<Args>(args)...);
+
+		if ((!_tree.key_comp()(*hint , *vp)) && (!_tree.key_comp()(*vp , *hint)))
+		{
+			_tree.get_allocator().deallocate(vp , 1);
+			return hint;
+		}
+
+		return (_tree.insert_unique(_STD move(*vp))).first;
+	}
+
+	iterator erase(iterator pos)//since C++17
+	{
+		return _tree.erase(pos);
+	}
+
+	iterator erase(const_iterator first , const_iterator last)
+	{
+		for (; first != last; ++first)
+			erase(first);
+		return last;
+	}
+
+	size_type erase(const key_type& key)
+	{
+		return _tree.deletekey(key);
+	}
+
+	void swap(set& right)
+		noexcept(_STD allocator_traits<Allocator>::is_always_equal::value&&_STD is_nothrow_swappable<Compare>::value)//since C++17
+	{
+		_STD swap(_tree , right._tree);
+	}
+
+	size_type count(const key_type& key) const
+	{
+		if (_tree.find(key) == nullptr)
+			return 0;
+		else
+			return 1;
+	}
+
+	iterator find(const key_type& key)
+	{
+		nodeptr np = _tree.find(key);
+		if (np == nullptr)
+			return cend();
+		else
+			return _tree.make_iter(np);
+	}
+
+	const_iterator find(const key_type& key) const
+	{
+		nodeptr np = _tree.find(key);
+		if (np == nullptr)
+			return cend();
+		else
+			return _tree.make_iter(np);
+	}
+
+	_STD pair<iterator , iterator> equal_range(const key_type& key)
+	{
+		return _STD make_pair(lower_bound(key) , upper_bound(key));
+	}
+
+	_STD pair<const_iterator , const_iterator> equal_range(const key_type& key) const
+	{
+		return _STD make_pair(lower_bound(key) , upper_bound(key));
+	}
+
+	iterator lower_bound(const key_type& key)
+	{
+		return _tree.lower_bound(key);
+	}
+
+	const_iterator lower_bound(const key_type& key) const
+	{
+		return _tree.lower_bound(key);
+	}
+
+	iterator upper_bound(const key_type& key)
+	{
+		return _tree.upper_bound(key);
+	}
+
+	const_iterator upper_bound(const key_type& key) const
+	{
+		return _tree.upper_bound(key);
+	}
+
+	key_compare key_comp() const
+	{
+		return _tree.key_comp();
+	}
+
+	value_compare value_comp() const
+	{
+		return _tree.key_comp();
+	}
+
+private:
+
+	template<typename Iter>
+	void _insert_range(Iter first , Iter last)
+	{
+		for (; first != last; ++first)
+			_tree.insert_unique(*first);
+	}
+
+	treetype _tree;
+};
+
+template<typename Key , typename Compare , typename Allocator>
+bool operator==(const set<Key , Compare , Allocator>& left ,
+	const set<Key , Compare , Allocator>& right)
+{
+	if (left.size() != right.size())
+		return false;
+	Compare comp(left.key_comp());
+	auto lit = left.begin();
+	auto rit = right.begin();
+	for (; lit != left.end();)
+	{
+		if ((!comp(*lit , *rit) && (!comp(*rit , *lit))))
+		{
+			++lit;
+			++rit;
+		}
+		else
+			return false;
+	}
+	return true;
+}
+
+template<typename Key , typename Compare , typename Allocator>
+bool operator!=(const set<Key , Compare , Allocator>& left ,
+	const set<Key , Compare , Allocator>& right)
+{
+	return !(left == right);
+}
+
+template<typename Key , typename Compare , typename Allocator>
+bool operator<(const set<Key , Compare , Allocator>& left ,
+	const set<Key , Compare , Allocator>& right)
+{
+	return _STD lexicographical_compare(left.begin() , left.end() , right.begin() , right.end() , left.key_comp());
+}
+
+template<typename Key , typename Compare , typename Allocator>
+bool operator>(const set<Key , Compare , Allocator>& left ,
+	const set<Key , Compare , Allocator>& right)
+{
+	return _STD lexicographical_compare(right.begin() , right.end() , left.begin() , left.end() , left.key_comp());
+}
+
+template<typename Key , typename Compare , typename Allocator>
+bool operator<=(const set<Key , Compare , Allocator>& left ,
+	const set<Key , Compare , Allocator>& right)
+{
+	return !(left > right);
+}
+
+template<typename Key , typename Compare , typename Allocator>
+bool operator>=(const set<Key , Compare , Allocator>& left ,
+	const set<Key , Compare , Allocator>& right)
+{
+	return !(left < right);
+}
+
+template<typename Key , typename Compare , typename Allocator>
+void swap(set<Key , Compare , Allocator>& left ,
+	set<Key , Compare , Allocator>& right) noexcept(noexcept(left.swap(right)))
+{
+	left.swap(right);
+}
